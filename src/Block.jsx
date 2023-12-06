84886f0a35693b7163b7aa177733abd6ecf56831
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2023-12-05 21:26:31 -0500

    feat: shape depends on the state

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,9 +1,18 @@
 import './Block.css'
 
 const Block = ({value}) => {
+  const colors = {
+    1: 'cyan',
+    2: 'blue',
+    3: 'orange',
+    4: 'red',
+    5: 'yellow',
+    6: 'green',
+    7: 'violet',
+  };
+
   return (
-    <div className="block">
-      {/*value*/}
+    <div className= {`block ${colors[value]}`}>
     </div>
   )
 }
